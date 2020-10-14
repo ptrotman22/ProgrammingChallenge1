274b4d8adbc01767870364d834fff2f9d477e902
Pierce Trotman <ptrotman22@gmail.com>
2020-10-14 11:06:10 -0400

    Adding testing to program

---
+++ src/Java/ProgrammingChallenge.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class ProgrammingChallenge {
+    public static void main(String[] args) throws IOException{
+        Charset utf8 = StandardCharsets.UTF_8;
+
+        List<String> list = Arrays.asList();
+    }
+}
